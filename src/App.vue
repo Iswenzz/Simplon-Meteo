ef49904e48931bcd1a0278cf3d51d50bb4b29533
Iswenzz <suxlolz1528@gmail.com>
2021-01-16 22:07:56 +0100

    App: responsive + more infos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
 	<div id="app">
 		<div class="page">
-			<h1>Simplon Météo</h1>
+			<h1 class="header">Simplon Météo</h1>
 			<input class="searchbar" type="text" v-model="city">
 			<div v-if="this.data" class="card">
-				<div>
+				<div class="card-icon">
+					<h4>{{ "Coords " + this.data.coord.lat + ", " + this.data.coord.lon }}</h4>
 					<img :src="`http://openweathermap.org/img/wn/${this.data.weather[0].icon}@4x.png`"
 						:alt="this.data.weather[0].description">
+					<h3>{{ "Min " + this.data.main.temp_min + "°C, Max " + this.data.main.temp_max + "°C" }}</h3>
 				</div>
-				<div>
-					<h3 class="card-main">{{ this.data.name }}</h3>
-					<h2 class="card-main">{{ this.data.weather[0].main }}</h2>
-					<h3 class="card-degree">{{ this.data.main.temp + "°C" }}</h3>
+				<div class="card-info">
+					<h2>{{ this.data.name }}</h2>
+					<time>{{ new Date().toLocaleString("fr-FR") }}</time>
+					<h2>{{ this.data.weather[0].main }}</h2>
+					<div>
+						<h3>{{ this.data.main.temp + "°C" }}</h3>
+						<h3>{{ "Ressentie " + this.data.main.feels_like + "°C" }}</h3>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -49,38 +55,104 @@ export default {
 </script>
 
 <style lang="scss">
+$queries: (
+	desktop: "min-width: 1224px",
+	tablet_mobile: "max-device-width: 1224px",
+	portrait: "orientation: portrait",
+	retina: "min-resolution: 2dppx",
+	big: "min-device-width: 1824px"
+);
+
+@mixin media($keys...) {
+	@each $key in $keys {
+		@media (map-get($queries, $key)) {
+			@content;
+		}
+	}
+}
+
 html, body, #app {
 	margin: 0;
 	padding: 0;
-	background: linear-gradient(90deg, #FFE53B 0%, #fd3838 74%);
+	background-color: #1c1c1c;
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	color: #fff;
 	text-align: center;
 }
 
+.header {
+	margin-top: 0;
+	padding: 0.8em;
+	background-color: #0e0e0e;
+}
+
 .card {
 	display: flex;
+	padding-top: 3em;
 	justify-content: center;
 	align-items: center;
-	flex-direction: column;
-	width: 500px;
+	width: 60%;
 	height: 400px;
-	max-width: 800px;
 	margin: auto;
-	background: #fff;
 	box-shadow: 0 14px 80px rgba(34, 35, 58, 0.2);
-	border-radius: 30px;
-	color: black;
+	color: white;
+	flex-direction: row;
+
+	#{&}-info {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		color: black;
+		background-color: white;
+		width: 100%;
+		height: 100%;
+		border-top-right-radius: 50px;
+		border-bottom-right-radius: 50px;
+
+		@include media(tablet_mobile, portrait) {
+			border-top-right-radius: 0;
+			border-bottom-right-radius: 50px;
+			border-bottom-left-radius: 50px;
+		}
+	}
+
+	#{&}-icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		background: linear-gradient(140deg, #0d23e7 0%, #00e1ff 74%);
+		width: 100%;
+		height: 100%;
+		border-top-left-radius: 50px;
+		border-bottom-left-radius: 50px;
+
+		@include media(tablet_mobile, portrait) {
+			border-top-right-radius: 50px;
+			border-top-left-radius: 50px;
+			border-bottom-left-radius: 0;
+		}
+	}
+
+	@include media(tablet_mobile, portrait) {
+		flex-direction: column;
+		width: 80%;
+	}
 }
 
 .searchbar {
-	width: 440px;
+	width: 40%;
 	height: 30px;
-	margin: 1em;
 	border: none;
+	margin: 1em;
 	border-radius: 30px;
 	color: #fff;
 	background: dimgray;
 	padding: 0 30px;
+
+	@include media(tablet_mobile, portrait) {
+		margin-bottom: 5em;
+	}
 }
 </style>
